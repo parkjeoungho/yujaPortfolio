094e117cf4ee5f7756f4f62dfd764787ca3d7117
parkjeoungho <alfhdoswlfh1024@gmail.com>
2023-10-06 03:54:15 +0900

    Updates

---
+++ _next/static/uxLqrmeIUwHouAlYzYrkS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/66-c8aedb1c7bcfa7a6.js","static/chunks/pages/index-2102cbf2c586d6b2.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
