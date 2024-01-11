73e82846238e2f9cf3c2d305bc1ec7a56522b133
parkjeoungho <alfhdoswlfh1024@gmail.com>
2024-01-12 00:46:50 +0900

    fix(/): 경로 수정

--- src/app.tsx
+++ src/app.tsx
@@ -23,17 +23,17 @@ export function App({ Component, pageProps }: AppProps) {
         <meta property="og:url" content="https://parkjeoungho.github.io/yujaPortfolio/" />
         <meta property="og:title" content="신유진의 포트폴리오입니다." />
         <meta property="og:description" content="디자이너 신유진의 포트폴리오입니다." />
-        <meta property="og:image" content={`${prefix}assets/images/yujin_thumbnail.jpg`} />
+        <meta property="og:image" content={`${prefix}/assets/images/yujin_thumbnail.jpg`} />
         <meta property="og:site_name" content="신유진의 포트폴리오" />
         <meta property="og:locale" content="ko_KR" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <link rel="shortcut icon" sizes="192x192" href={`${prefix}app_icon.png`} />
+        <link rel="shortcut icon" sizes="192x192" href={`${prefix}/app_icon.png`} />
         <meta name="apple-mobile-web-app-title" content="신유진의 포트폴리오" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <link rel="apple-touch-icon" sizes="128x128" href={`${prefix}app_icon.png`} />
-        <link rel="apple-touch-icon-precomposed" sizes="128x128" href={`${prefix}app_icon.png`} />
+        <link rel="apple-touch-icon" sizes="128x128" href={`${prefix}/app_icon.png`} />
+        <link rel="apple-touch-icon-precomposed" sizes="128x128" href={`${prefix}/app_icon.png`} />
         <meta name="theme-color" content="#4285f4" />
         <link rel="icon" href={`${prefix}/favicon.ico`} />
         <title>신유진의 포트폴리오입니다.</title>
